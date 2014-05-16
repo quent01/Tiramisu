23bc3a8e8abe371d6fbb4bca61c94350a6737b21
quent01 <quentinthenoz@gmail.com>
2014-05-17 00:44:51 +0200

    Navigation responsive

--- templates/_header.php
+++ templates/_header.php
@@ -2,13 +2,15 @@
 	<div class="bandeau bg_yellow shadow"> </div>
 	<nav id="navbar_principal">
 		<div class="logo">
-			<img src="img/logo_tiramisu.png"/>
-			<a href="index.php">Tiramisù</a>
+			<a href="index.php">
+				<img src="img/logo_tiramisu.png"/>
+				<span>Tiramisù</span>
+			</a>
 		</div> 
-		<ul id="nav_principal">
-			<li><a href="#">equipe</a></li>
-			<li><a href="#">archives</a></li>
-			<li><a href="#">bonus</a></li>
-		</ul>
+		<ul id="nav_principal"><!--
+			--><li><a href="#">equipe</a></li><!--
+			--><li><a href="#">archives</a></li><!--
+			--><li><a href="#">bonus</a></li><!--
+	--></ul>
 	</nav>
 </header>
